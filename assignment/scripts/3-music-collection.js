e1c3d3c0d90d12e057385946b3cba4ea146b6ca9
Kongxib Vue <kongcvue@gmail.com>
2021-09-17 13:28:48 -0500

    wil add to collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,24 @@
 console.log('***** Music Collection *****')
+
+let collection = [];//empty array for music collection
+
+function addToCollection(title, artist, yearPublished){
+  let album = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished,
+  }
+
+  collection.push(album);
+  return album;
+}
+
+console.log(addToCollection('Certified Lover Boy', 'Drake', 2021));
+console.log(addToCollection('Head in the Clouds II', '88rising', 2019));
+console.log(addToCollection('Volume 2', 'Pink Sweat$', 2019));
+console.log(addToCollection('The Universe Smiles Upon You', 'Khurangbin', 2015));
+console.log(addToCollection('Californication', 'Red Hot Chili Peppers', 1999));
+console.log(addToCollection('rainy evening', 'Idealism', 2017));
+
+
+console.log(collection);
